75a71ff9fddc36c3485c6ebba828036c1f50a18e
tdduy <tdduy88@gmail.com>
2014-10-09 16:56:49 +0700

    init project

---
+++ primefaces_component-portlet/WEB-INF/src/com/prime/test/bean/GreenBean.java
@@ -0,0 +1,22 @@
+package com.prime.test.bean;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+
+@ManagedBean(name = "GreenBean")
+@ViewScoped
+public class GreenBean implements Serializable {
+	private static final long serialVersionUID = 2802078677405999646L;
+	private String helloWord = "Wellcome to Primeface";
+
+	public String getHelloWord() {
+		return helloWord;
+	}
+
+	public void setHelloWord(String helloWord) {
+		this.helloWord = helloWord;
+	}
+}
