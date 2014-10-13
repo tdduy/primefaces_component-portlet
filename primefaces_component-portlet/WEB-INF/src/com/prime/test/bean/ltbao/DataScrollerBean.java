a8ce25d212ddf89d28d5a103f4e7f822ff84702c
ltbao472 <ltbao472@gmail.com>
2014-10-13 18:10:27 +0700

    add DataScrollerBean portlet

---
+++ primefaces_component-portlet/WEB-INF/src/com/prime/test/bean/ltbao/DataScrollerBean.java
@@ -0,0 +1,14 @@
+package com.prime.test.bean.ltbao;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean(name="DataScrollerBean")
+@ViewScoped
+public class DataScrollerBean {
+	
+	public class Model{
+	
+		
+	}
+}
